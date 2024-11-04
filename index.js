728148bc66b5882ca4c192db5160f94b0d520f42
kamiokz123 <kamranhaiderokz888@gmail.com>
2024-11-04 02:48:18 -0800

    added crud operations

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const methodOverride = require('method-override');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -17,27 +26,134 @@ const getRandomUser = () => {
   ];
 }
 
-// let q = "INSERT INTO USER (id,username,email,password) VALUES (?,?,?,?)"; single per data
-// let user = ["1","abc1","abc@gmail.com","abc"]; single per data
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) FROM user";
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
+    });
+  } catch (error) {
+    res.send("something wrong");
+    console.log(error);
+  }
+});
 
-let q = "INSERT INTO USER (id,username,email,password) VALUES ?"; // double person or more data
-let data = [];
+app.get("/users", (req, res) => {
+  let q = "SELECT id,username,email FROM user";
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      let data = result;
+      console.log(data[0]);
+      res.render("users.ejs", { data });
+    });
+  } catch (error) {
+    res.send("something wrong");
+    console.log(error);
+  }
+});
 
-for (let i = 1; i <= 100; i++) {
-  data.push(getRandomUser());
-}
+
+app.get("/users/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id="${id}"`;
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      let user = result[0];
+      console.log(user);
+      res.render("edit.ejs", { user })
+    });
+  } catch (error) {
+    res.send("something wrong");
+    console.log(error);
+  }
+});
+
+app.patch("/users/:id/update", (req, res) => {
+  let { id } = req.params;
+  let { password: formPass, username: newUsername } = req.body;
+  console.log(formPass, newUsername);
+
+  let q = `SELECT * FROM user WHERE id="${id}"`;
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      let user = result[0];
+      if (formPass != user.password) {
+        res.send("wrong password")
+      } else {
+        let q2 = `UPDATE user SET username="${newUsername}" WHERE id="${user.id}"`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          console.log(result);
+          res.redirect("/users");
+        })
+      }
+    });
+  } catch (error) {
+    res.send("something wrong in database");
+    console.log(error);
+  }
+});
 
 
-try {
-  connection.query(q, [data], function (err, results, fields) {
-    if (err) throw err;
-    console.log("res :", results);
-  });
-} catch (error) {
-  console.log(error);
-}
+app.get("/add", (req, res) => {
+  res.render("add.ejs")
+});
 
-connection.end();
+app.post("/add", (req, res) => {
+  let id = uuidv4();
+  let { username, email, password } = req.body;
+  console.log(username, email, password, id);
+
+  let q = `INSERT INTO user (id, username, password, email) VALUES ("${id}","${username}", "${password}", "${email}")`;
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.redirect("/users")
+    });
+  } catch (error) {
+    res.send("something wrong in database");
+    console.log(error);
+  }
+});
+
+
+app.delete("/users/:id/delete",(req,res)=>{
+  let {id} = req.params;
+  console.log(id);
+  let q = `DELETE FROM user WHERE id = "${id}"`;
+  try {
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.redirect("/users")
+    });
+  } catch (error) {
+    res.send("something wrong in database");
+    console.log(error);
+  }  
+});
+
+app.listen(8080, () => {
+  console.log("app is listening");
+})
+
+
+// try {
+//   connection.query(q, [data], function (err, results, fields) {
+//     if (err) throw err;
+//     console.log("res :", results);
+//   });
+// } catch (error) {
+//   console.log(error);
+// }
+
+// connection.end();
 
 
 
